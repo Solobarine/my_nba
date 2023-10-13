c753047fe82c9618af08410ac36d7deb2ae42054
Solly <solobarine@gmail.com>
2023-10-13 11:22:38 +0100

    Create Team Props Interface

---
+++ src/interface/teamProps.ts
@@ -0,0 +1,19 @@
+export interface TeamProps {
+  strTeam: string
+  strTeamShort: string
+  strStadium: string
+  strStadiumThumb: string
+  strStadiumDescription: string
+  strWebsite: string
+  strFacebook: string
+  strTwitter: string
+  strInstagram: string
+  strDescriptionEN: string
+  strTeamBadge: string
+  strTeamJersey: string
+  strTeamLogo: string
+  strTeamFanart1: string
+  strTeamFanart2: string
+  strTeamFanart3: string
+  strYoutube: string
+}
