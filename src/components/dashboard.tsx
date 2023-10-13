b7f3269a5de7b995c00648e3b4059e913cb7e5ee
Solly <solobarine@gmail.com>
2023-10-13 12:06:54 +0100

    Create Simple Dash Component

---
+++ src/components/dashboard.tsx
@@ -0,0 +1,13 @@
+import styles from './dashboard.module.css'
+
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <section className={styles.main}>
+      <h1 className={styles.head}>MY NBA</h1>
+    </section>
+  )
+}
+
+export default Dashboard
